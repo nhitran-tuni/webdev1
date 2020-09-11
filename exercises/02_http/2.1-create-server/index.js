20caeffe5ae67ca0cf19b26f6db77e86014d36b5
Nhi Tran <thuyphuongnhi.tran@tuni.fi>
2020-09-12 00:25:39 +0300

    modify err

--- exercises/02_http/2.1-create-server/index.js
+++ exercises/02_http/2.1-create-server/index.js
@@ -3,7 +3,6 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     fs.readFile('index.html', (err, data) => {
-        if (err) throw err;
         response.writeHead(200, {'Content-Type' : 'text/html'});
         response.write(data);
         // response.end();
