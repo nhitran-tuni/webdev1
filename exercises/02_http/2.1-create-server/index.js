5a0f3be8c7460726bd37e968bb81b45acdce7392
Nhi Tran <thuyphuongnhi.tran@tuni.fi>
2020-09-12 00:31:17 +0300

    modify error

--- exercises/02_http/2.1-create-server/index.js
+++ exercises/02_http/2.1-create-server/index.js
@@ -2,12 +2,11 @@ const fs = require('fs');
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-    fs.readFile('index.html', (err, data) => {
-        response.writeHead(200, {'Content-Type' : 'text/html'});
+    fs.readFile('./index.html', (err, data) => {
+        response.writeHead(200, {'Content-Type' : "text/html"});
         response.write(data);
-        // response.end();
+        response.end();
     });
-    response.end();
 });
 
 server.listen(3000, '127.0.0.1');
