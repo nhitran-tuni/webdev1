2409ff2e9cd0b5f2649166301be4586a316d6edf
Nhi Tran <thuyphuongnhi.tran@tuni.fi>
2020-09-13 13:08:24 +0300

    test 2.4

---
+++ exercises/02_http/2.4-content-types/index.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const fs = require('fs');
+
+const acceptHeaders ={
+    "*/*" : "data.txt",
+    "text/plain" : "data.txt",
+    "application/json" : "data.json",
+    "application/xml" : "data.xml",
+    "text/xml" : "data.xml"
+}
+
+const server = http.createServer((req, res) =>{
+    const reqHeader = req.headers.accept;
+
+    if (acceptHeaders.hasOwnProperty(reqHeader)) {
+        fs.readFile(acceptHeaders.reqHeader, (err, data) => {
+            res.writeHead (200, {'Content-Type' : reqHeader});
+            res.write(data);
+            res.end();
+        });
+    } else {
+        res.statusCode = 406;
+        res.statusMessage = 'Content type not available';
+        // res.writeHead (406, 'Content type not available');
+        res.end();
+    }
+}).listen(3000, '127.0.0.1');
