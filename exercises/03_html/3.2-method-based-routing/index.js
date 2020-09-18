dc628c808f42bbad57e7ee2d231c0610b6f55828
Nhi Tran <thuyphuongnhi.tran@tuni.fi>
2020-09-18 20:12:57 +0300

    test 3.2

---
+++ exercises/03_html/3.2-method-based-routing/index.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const fs = require('fs');
+
+const accMethod = {
+    "GET" : "get.html",
+    "POST" : "post.html"
+}
+
+const server = http.createServer((req, res) => {
+    const {method, url} = req;
+
+    if(Object.keys(accMethod).includes(method)) {
+        fs.readFile(accMethod[method], (err, data) => {
+            res.writeHead(200, {'Content-Type' : 'text/html'});
+            res.write(data);
+            res.end();
+        });
+    } else {
+        res.writeHead(405, 'Allow: GET, POST');
+        res.end;
+    };
+}).listen(3000);
