ddb30592d020d17b3efaf2290db017dca307f5fe
Nhi Tran <thuyphuongnhi.tran@tuni.fi>
2020-09-19 17:07:21 +0300

    modify 3.5

--- exercises/03_html/3.5-html-template-tag/template.js
+++ exercises/03_html/3.5-html-template-tag/template.js
@@ -1,19 +1,15 @@
-const name = document.getElementById("input-name").value;
-const email = document.getElementById("input-email").value;
-const homepage = document.getElementById("input-homepage").value;
-
 document.getElementById("submit").addEventListener("click", (event) => {
     event.preventDefault();
     const clone = document.getElementById("contact-template").content.cloneNode(true);
 
-    clone.querySelector("h2").innerHTML = name;
-    clone.querySelector("p").innerHTML = email;
+    clone.querySelector("h2").innerHTML = document.getElementById("input-name").value;
+    clone.querySelector("p").innerHTML = document.getElementById("input-email").value;
 
-    var newHomepage = clone.querySelector("a");
+    const newHomepage = clone.querySelector("a");
+    const homepage = document.getElementById("input-homepage").value;
     newHomepage.setAttribute("href", homepage);
     newHomepage.innerHTML = homepage;
 
     document.getElementById("contacts").appendChild(clone);
     document.getElementById("form").reset(); 
-    console.log('a');
 })
